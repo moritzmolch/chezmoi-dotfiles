d9cbce5579c21f0daaac5c0aa31b7cb4a88f7f2e
moritzmolch <moritz.molch@kit.edu>
2025-10-27 12:41:27 +0100

    [neovim] Add resources used when creating this config

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -3,6 +3,10 @@
 ---
 --- Neovim initialization file
 ---
+--- Resources used to create this configuration:
+--- - https://github.com/schuam/.dotfiles/blob/main/.config/nvim
+--- - https://github.com/weeman1337/ansible-workstation/tree/main/roles/neovim/files/config
+---
 
 
 -- Load module with basic editor settings
