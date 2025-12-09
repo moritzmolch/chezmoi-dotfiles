8c58f128e1af2c38f516138eaae5dfee8461a847
moritzmolch <moritz.molch@kit.edu>
2025-12-09 09:39:33 +0100

    [neovim] Add vimwiki

---
+++ private_dot_config/nvim/lua/core/_plugins/vimwiki.lua
@@ -0,0 +1,21 @@
+--
+-- lua/core/_plugins/vimwiki.lua
+--
+-- Personal documentation wiki
+--
+
+return {
+    "vimwiki/vimwiki",
+    keys = { "<leader>ww", "<leader>wt" },
+    init = function ()
+        vim.g.vimwiki_list = {
+            {
+                path = "~/cernbox/vimwiki",
+                syntax = "markdown",
+                ext = "md",
+            },
+        }
+        vim.g.vimwiki_ext2syntax = {  }
+        vim.g.vimwiki_global_ext = 0
+    end,
+}
